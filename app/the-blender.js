093b21db5d568e3832153f61771e4157a03617f3
vdaguenet <valentin.daguenet@gmail.com>
2014-05-26 15:31:11 +0200

    Add dependencies

--- app/the-blender.js
+++ app/the-blender.js
@@ -2,7 +2,12 @@
  * Module dependencies.
  */
 var Server = require('./lib/server').Server,
-	Blender = require('./lib/blender').Blender,
+	Blender = require('./lib/blender/blender').Blender,
+	Master = require('./lib/blender/master').Master,
+	Pourer = require('./lib/blender/pourer').Pourer,
+	Recipe = require('./lib/recipe/recipe').Recipe,
+	Step = require('./lib/recipe/step').Step,
+	Parameter = require('./lib/recipe/parameter').Parameter,
     Five   = require('johnny-five');
 
 // Instanciate the server.
