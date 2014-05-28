58aacc396e64684a6551c663a3889a083cfc467c
vdaguenet <valentin.daguenet@gmail.com>
2014-05-28 13:10:28 +0200

    Starting API to make the angular app and the node one communicate.

--- app/the-blender.js
+++ app/the-blender.js
@@ -15,3 +15,27 @@ var server = new Server();
 
 // Start the server.
 server.init(5555);
+
+/**
+ * Setting the routes
+ */
+// The API to communicate with the Angular app
+server.app.get('/api/blender/recipes', function(req, res) {
+	// Get every recipes available in the blender
+	console.log('These are my recipes.');
+});
+
+server.app.post('/api/blender/recipes/:recipe_uid', function(req, res) {
+	// Save a new recipe in the blender
+	console.log('Oh a new recipe ! Thank you my lord');
+});
+
+server.app.delete('/api/blender/recipes/:recipe_uid', function(req, res) {
+	// Delete a recipe in the blender
+	console.log('Bye bye sweet recipe ...');
+});
+
+server.app.post('/api/blender/execute/:recipe_uid', function(req, res) {
+	// Ask the blender to create a recipe (already saved or created by user)
+	console.log('More work ! Yes my lord !');
+});
