2479dc212d2d10f1d3b2b87881034be279b5bd71
vdaguenet <valentin.daguenet@gmail.com>
2014-05-28 15:25:32 +0200

    Implementing few functions

--- public/js/services/blenderServices.js
+++ public/js/services/blenderServices.js
@@ -6,17 +6,24 @@ var bs = angular.module('blenderService', []);
 // Service for recipes management
 bs.factory('Recipes', ['$http', function($http) {
 	return {
-		get: function() {
+		getAll: function() {
 			// Some code to get the recipes
+			return $http.get('/api/blender/recipes');
+		},
+		get: function(uid) {
+			// Some code to get the recipes
+			return $http.get('/api/blender/recipes/' + uid);
 		},
 		create: function() {
 			// Some code to create a recipe
 		},
 		save: function() {
 			// Some code to save a recipe in The Blender
+			return $http.post('/api/blender/recipes/' + uid);
 		},
 		delete: function() {
 			// Some code to delete a recipe
+			return $http.delete('/api/blender/recipes/' + uid);
 		}
 	}
 }]);
