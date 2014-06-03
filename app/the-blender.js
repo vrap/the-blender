d703e526212fe3a614bbacec5f624df808f23ee4
FPierre <pflauder@gmail.com>
2014-06-03 23:17:53 +0200

    Beginning of the plateau implementation

--- app/the-blender.js
+++ app/the-blender.js
@@ -2,19 +2,23 @@
  * Module dependencies.
  */
 
+var Five  = require('johnny-five'),
+    board = new Five.Board({
+        port: '/dev/ttyACM0'
+    });
+
 board.on('ready', function() {
-    var Server = require('./lib/server').Server,
-    Blender = require('./lib/blender/blender').Blender,
-    Plateau = require('./lib/blender/plateau').PLateau,
-    Pourer = require('./lib/blender/pourer').Pourer,
-    Recipe = require('./lib/recipe/recipe').Recipe,
-    Step = require('./lib/recipe/step').Step,
-    Parameter = require('./lib/recipe/parameter').Parameter,
-    Five   = require('johnny-five'),
-    mongoose = require('mongoose'),
-    http = require('http'),
-    database = require('../config/database'),
-    Version = require('./lib/version');
+    var Server    = require('./lib/server').Server,
+        Blender   = require('./lib/blender/blender').Blender,
+        Plateau   = require('./lib/blender/plateau').Plateau,
+        Pourer    = require('./lib/blender/pourer').Pourer,
+        Recipe    = require('./lib/recipe/recipe').Recipe,
+        Step      = require('./lib/recipe/step').Step,
+        Parameter = require('./lib/recipe/parameter').Parameter,
+        mongoose  = require('mongoose'),
+        http      = require('http'),
+        database  = require('../config/database'),
+        Version   = require('./lib/version');
 
     var masterUri = "http://localhost:8080/LP-DevWeb/The%20Blender/the-blender-master";
 
