c71870abc06126c4444a12c8879912de623f743b
vdaguenet <valentin.daguenet@gmail.com>
2014-06-05 14:25:34 +0200

    Implement pour method

--- app/lib/blender/pourer.js
+++ app/lib/blender/pourer.js
@@ -1,5 +1,6 @@
 var util = require('util'),
-	Module = require('./module').Module;
+	Module = require('./module').Module,
+    Five    = require('johnny-five');
 
 /**
  * Pourer Module prototype
@@ -14,18 +15,29 @@ util.inherits(Pourer, Module);
  * Initialization of the pourer
  *
  * @param order
- * @param components
- * @param liquid
+ * @param pin
+ * @param board
  */
-Pourer.prototype.init = function(order, components, liquid) {
+Pourer.prototype.init = function(order, pin, board) {
 	this.order = order;
-	this.components = components;
-	this.liquid = liquid;
+	this.components = {
+        servo: new Five.Servo({
+            pin: pin,
+            center: true
+        }),
+        board: board
+    };
 };
 
 /**
  * Call actuators to pour some liquid
  */
-Pourer.prototype.pour = function() {
+Pourer.prototype.pour = function(pourTime, nbPour) {
 	// Actions to pour the liquid
+    var posBottom   = 179;
+
+    this.components.servo.to(posBottom);
+    this.components.board.wait(nbPour*pourTime, function() {
+        this.components.servo.center();
+    }.bind(this));
 };
