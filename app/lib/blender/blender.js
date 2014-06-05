8dab4356f54504f77e8f78135f6b1b613cedc067
vdaguenet <valentin.daguenet@gmail.com>
2014-06-05 16:44:12 +0200

    Begining of recipe execution

--- app/lib/blender/blender.js
+++ app/lib/blender/blender.js
@@ -1,3 +1,7 @@
+var mongoose = require('mongoose'),
+    Pourer = require('./pourer').Pourer,
+    ModuleModel = require('../model/module');
+
 /**
  * Blender prototype
  * @type {Blender}
@@ -6,8 +10,40 @@ exports.Blender = Blender = function () {};
 
 /**
  * Initializes the Blender with its modules or set it as an object
- * @param {Object} modules
+ * @param {Array} modules
  */
 Blender.prototype.init = function (modules) {
-	modules = typeof modules !== 'undefined' ?  this.modules = modules : this.modules = {};
+	modules = typeof modules !== 'undefined' ?  this.modules = modules : this.modules = [];
+};
+
+/**
+ * Execute steps to have a cocktail
+ * @param {Array} steps
+ */
+Blender.prototype.execute = function(steps) {
+    for(var i in steps) {
+        console.log(steps[i]);
+        switch(steps[i].action) {
+            case 'poor': console.log('POOR !');
+                var ingrUuid,
+                    module,
+                    pourer = new Pourer(),
+                    nbPour;
+
+                for(j in steps[i].parameters) {
+                    if(steps[i].parameters[j].name == 'ingredient') {
+                        ingrUuid = steps[i].parameters[j].value;
+                    }
+                    if(steps[i].parameters[j].name == 'dosage') {
+                        nbPour = steps[i].parameters[j].value;
+                    }
+                }
+
+                ModuleModel.findOne({content: ingrUuid}, function(err, data) {
+                   pourer.init(data.order);
+                });
+
+                break;
+        }
+    }
 };
