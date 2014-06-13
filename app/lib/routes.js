d66c04fdb07fa7d3986455e96bea24f9305d2b2f
Romain <r.esilva@sysdream.com>
2014-06-13 02:08:40 +0200

    Remove express routes logic that does not suit the new architecture

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -1,56 +1,39 @@
-var mongoose  	= require('mongoose'),
-    Blender     = require('./blender/blender').Blender,
+var mongoose = require('mongoose'),
+    Blender = require('./blender').Blender,
     ModuleModel = require('./model/module'),
-	RecipeModel = require('./model/recipe');
+    RecipeModel = require('./model/recipe');
 
 /**
  * Routes for the application
  */
 module.exports = function(app) {
-	// The API to communicate with the Angular app
-	app.get('/api/blender/recipes', function(req, res) {
-		
-		// Get every recipes available in the blender
-		RecipeModel.find(function(err, data){
-			res.send(data);
-		});
+    // The API to communicate with the Angular app
+    app.get('/api/blender/recipes', function(req, res) {
 
-	});
-
-	app.get('/api/blender/recipes/:recipe_uid', function(req, res) {
-		// Get a recipe available in the blender
-		res.send('This is the recipe.');
-	});
-
-	app.post('/api/blender/recipes/:recipe_uid', function(req, res) {
-		// Save a new recipe in the blender
-		res.send('Oh a new recipe ! Thank you my lord');
-	});
-
-	app.delete('/api/blender/recipes/:recipe_uid', function(req, res) {
-		// Delete a recipe in the blender
-		res.send('Bye bye sweet recipe ...');
-	});
-
-	app.post('/api/blender/execute', function(req, res) {
-		// Ask the blender to create a recipe (already saved or created by user)
-        var blender = new Blender(),
-            modules,
-            body = "";
-
-        ModuleModel.find(function(err, data) {
-            modules = data;
-            blender.init(modules);
-
-            req.on('data', function (chunk) {
-                body += chunk;
-            });
-            req.on('end', function () {
-                var a = JSON.parse(body);
-                blender.execute(a.steps);
-
-                res.send('More work ! Yes my lord !');
-            });
+        // Get every recipes available in the blender
+        RecipeModel.find(function(err, data) {
+            res.send(data);
         });
-	});
+
+    });
+
+    app.get('/api/blender/recipes/:recipe_uid', function(req, res) {
+        // Get a recipe available in the blender
+        res.send('This is the recipe.');
+    });
+
+    app.post('/api/blender/recipes/:recipe_uid', function(req, res) {
+        // Save a new recipe in the blender
+        res.send('Oh a new recipe ! Thank you my lord');
+    });
+
+    app.delete('/api/blender/recipes/:recipe_uid', function(req, res) {
+        // Delete a recipe in the blender
+        res.send('Bye bye sweet recipe ...');
+    });
+
+    app.post('/api/blender/execute', function(req, res) {
+        // Ask the blender to create a recipe (already saved or created by user)
+        res.send('Give my my cocktail please !');
+    });
 };
